800fe9b6e0c2a11b3f4fce92fb7e84907e8b4d79
Sachin-2006 <sachin06kaizen@gmail.com>
2025-08-13 09:40:19 +0530

    feat: Implement encapsulation and basic attendance recording (Part 4)

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -1,10 +1,38 @@
 package  com.school;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student("Laser");
-        Course c = new Course("Full Stack Developer");
-        s.displayDetails();
-        c.displayDetails();
-        
+
+        Student[] students = {
+            new Student("Ram"),
+            new Student("Sita"),
+            new Student("Lakshman"),
+            new Student("Hanuman")
+        };
+
+        Course[] courses = {
+            new Course("Full Stack Developer"),
+            new Course("Data Science"),
+            new Course("Cloud Computing"),
+            new Course("Blockchain Development")
+        };
+
+        ArrayList<AttendanceRecord> attendanceLog = new ArrayList<>();
+
+        for(int i =0;i<4;i++){
+            String pres = i%2==0?"Present":"Absent";
+            if(i == 3){
+                pres = "aa";
+            }
+            attendanceLog.add(new AttendanceRecord(students[i].getStudentId(),courses[i].getCourseId(),pres));
+        }
+        System.out.println("");
+
+        System.out.println("Attendance Records:");
+        System.out.println("");
+        for(AttendanceRecord a:attendanceLog){
+            a.displayRecord();
+        }
     }
 }
